07de372cb7af8d2c98069716a2be0f82bf12b08b
Prashant Sani <prashantsani@users.noreply.github.com>
2025-05-04 20:31:57 +0530

    style: css first tw4 config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Barlow } from "next/font/google";
-import "./globals.css";
+import "../styles/globals";
 
 const geistSans = Barlow({
   variable: "--font-barlow",
